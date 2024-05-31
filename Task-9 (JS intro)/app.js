7c0d7621ed025a91ac34a784e53c21eab92ab1e1
elminjanaliyev <elmincaneliyev@gmail.com>
2024-05-31 10:36:00 +0400

    add JS

---
+++ Task-9 (JS intro)/app.js
@@ -0,0 +1,36 @@
+//Task-1
+alert("My name is Elmin")
+//Task-2
+let userName1 = "Name: Elmin"
+console.log(userName1)
+//task-4
+let bookPrice = 750;
+let bookAmount = 14;
+console.log("Depodakı bütün kitabların cəmi:" , (bookPrice + bookAmount))
+//task-5
+let firstName = "Elmin";
+let lastName = " Canəliyev";
+alert("Mənim Adım: " + (firstName + lastName) + " Tanışlıgımza Şad oldum")
+//Task-9
+let fullName = "John Doe";
+let number = fullName.length
+console.log("Həriflərin sayı: " + number);
+//Task-10
+let uName = "Elmin Caneliyev"
+let fName = uName[0]
+let lName = uName[6]
+console.log("Ad Soyad: " + fName + lName )
+//task-11
+let userName = prompt("Salam, [ad]!")
+alert(userName)
+//task12
+let firstNum  = prompt("birinci ededi daxil edin")
+let secondNum = prompt("ikinci ededi daxil edin")
+let toplama = firstNum + secondNum
+console.log("Toplama:" + toplama)
+let vurma = firstNum * secondNum
+console.log("Vurma:" + vurma)
+let cixma = firstNum - secondNum
+console.log("cixma:" + cixma)
+let bolme = firstNum / secondNum
+console.log("bolme:" + bolme)
