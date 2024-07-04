85ab942922168fe29dafff891ca9ff5b10b345e6
elminjanaliyev <elmincaneliyev@gmail.com>
2024-07-05 01:27:19 +0400

    add new task

---
+++ Task-19 (JS DOM-2)/app.js
@@ -0,0 +1,19 @@
+ let blogs1= document.getElementById("blog-1")
+   blogs1.onclick = () =>{
+    document.body.style.backgroundColor = "green"
+   }
+
+   let blogs2= document.getElementById("blog-2")
+   blogs2.onclick = () =>{
+    document.body.style.backgroundColor = "yellow"
+   }
+
+   let blogs3= document.getElementById("blog-3")
+   blogs3.onclick = () =>{
+    document.body.style.backgroundColor = "blue"
+   }
+
+   let blogs4= document.getElementById("blog-4")
+   blogs4.onclick = () =>{
+    document.body.style.backgroundColor = "red"
+   }
