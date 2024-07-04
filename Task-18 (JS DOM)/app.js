26ebe51319fb05f2a7e8eae02569e938677bc5c7
elminjanaliyev <elmincaneliyev@gmail.com>
2024-07-04 17:23:26 +0400

    Task-DOM-1

---
+++ Task-18 (JS DOM)/app.js
@@ -0,0 +1,18 @@
+//Task-1
+//console.log(document.body.children[0])
+
+//Task-2
+//document.body.children[0].remove()
+//document.querySelector("#list").textContent = "TITAN RTX"
+
+//Task-3
+//document.body.children[0].textContent = "GeForce 20 Series"
+//document.querySelectorAll(".primary").forEach((element) => element.style.color = "#ebebeb")
+
+//Task-4
+//console.dir(document.querySelectorAll(".primary"));
+
+//Task-5
+
+//??????????
+
